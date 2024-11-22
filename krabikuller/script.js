f75c6e9eb7b81a002d0c10c1161506b9e53f5141
K Krabbi <krabypoiss@hotmail.com>
2024-11-22 20:21:18 +0200

    udate htms and js

--- krabikuller/script.js
+++ krabikuller/script.js
@@ -1,5 +1,26 @@
-import { getDatabase, ref, set, get, child, push } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+//import { getDatabase, ref, set, get, child, push } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPPfEB46a1d_gxWknguGbbiGNNODjflhA",
+  authDomain: "krabikuller.firebaseapp.com",
+  databaseURL: "https://krabikuller-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "krabikuller",
+  storageBucket: "krabikuller.firebasestorage.app",
+  messagingSenderId: "124331997863",
+  appId: "1:124331997863:web:1a025488223010e08ef982",
+  measurementId: "G-MT2PJRBTRZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 document.addEventListener('DOMContentLoaded', function () {
  /*    const ordersList = document.getElementById('ordersList');
     const dbRef =  firebase.database().ref('orders'); // Firebase'i andmebaasi viide
